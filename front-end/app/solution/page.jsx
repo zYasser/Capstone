64aa12744ae9f64027b4972565972a308c1e074e
yasserqamhiah <yaserqamhiah@gmail.com>
2024-04-12 13:05:23 +0300

    Pie Chart

--- front-end/app/solution/page.jsx
+++ front-end/app/solution/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import CostChart from "@/components/CostChart";
+import CostPieChart from "@/components/CostPie";
 import StatsCards from "@/components/StatsCards";
 import { useEffect } from "react";
 import { useState } from "react";
@@ -12,8 +13,8 @@ export default function Solution() {
   }, []);
 
   return (
-    <div className="bg-gray-200 min-h-screen">
-      <div>
+    <div className="bg-gray-200 min-h-screen flex justify-center">
+      <div className="w-4/5">
         <div className="">
           <h1 className="text-center text-black font-Roboto font-bold text-2xl pt-10 mb-10">
             Solution
@@ -22,10 +23,15 @@ export default function Solution() {
         {domLoaded && (
           <div className="">
             <StatsCards />
-            <div className="flex justify-center bg-white w-fit shadow-lg">
-              <CostChart
-                costs={{ cost: 300000, profit: 3000 * 12, years: 10 }}
-              ></CostChart>
+            <div className="flex justify-center  w-full mx-4 ">
+              <div className="bg-white mr-14  shadow-lg w-3/5">
+                <CostChart
+                  costs={{ cost: 300000, profit: 3000 * 12, years: 10 }}
+                ></CostChart>
+              </div>
+              <div className="bg-white shadow-lg h-fit w-fit">
+                <CostPieChart></CostPieChart>
+              </div>
             </div>
           </div>
         )}
